7e2063162208ef3b485e3939c9ada449d5c29499
Lance <lanceyu619@gmail.com>
2022-05-17 18:03:04 +0000

    added page for url_news to submit urls to be shortened

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8070; // default port 8080
 app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 
@@ -11,9 +13,17 @@ const urlDatabase = {
   "testing": "http://www.example.edu"
 };
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: req.param.longURL };
   res.render("urls_show", templateVars);
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.get("/urls", (req, res) => {
@@ -24,15 +34,6 @@ app.get("/urls", (req, res) => {
 
 app.get("/", (req, res) => {
   res.send("Hello!");
-});
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
-
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
 });
 
 app.get("/hello", (req, res) => {
@@ -48,3 +49,32 @@ app.get("/set", (req, res) => {
  app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
  });
+ 
+ app.get("/u/:shortURL", (req, res) => {
+  // const longURL = ...
+  res.redirect(longURL);
+});
+
+
+ app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send(generateRandomString());         // Respond with 'Ok' (we will replace this)
+});
+
+
+
+function generateRandomString() {
+  let shortURL = '';
+  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+  for (let i = 0; shortURL.length < 6; i++) {
+    let tmpStr = possible.charAt(Math.floor(Math.random() * 54 ));
+    tmpStr = tmpStr.replace(/[&/\#,+()$~%.;`^'":[]*?<_>=@{}]/, 'q');
+    shortURL += tmpStr;
+  } 
+  return shortURL;
+}
