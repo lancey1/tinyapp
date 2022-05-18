cdf4228035ae71be56b4e8010d3e71743a2a452f
Lance <lanceyu619@gmail.com>
2022-05-18 04:29:56 +0000

    added login logout function will need some CSS styling clean up

--- express_server.js
+++ express_server.js
@@ -27,14 +27,14 @@ const generateRandomString = function() {
 
 app.get("/urls/new", (req, res) => {
   let username = req.cookies["username"];
-  const templateVars = {username:username };
+  const templateVars = {username};
   res.render("urls_new",templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   let username = req.cookies["username"];
   let shortURL = req.params.shortURL;
-  const templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL],username:username };
+  const templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL],username};
   res.render("urls_show", templateVars);
 });
 
@@ -44,7 +44,7 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/urls", (req, res) => {
   let username = req.cookies["username"];
-  const templateVars = { urls: urlDatabase, username: username };
+  const templateVars = { urls: urlDatabase, username};
   res.render("urls_index", templateVars);
 });
 
@@ -82,12 +82,14 @@ app.post("/login", (req, res) => {
   res.redirect('/urls');
 });
 
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect('/urls');
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
-
 
 
 app.get("/hello", (req, res) => {
